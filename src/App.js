4244ca07855e99a8bff6ffb12441c6b637f8c84d
Sarunas Medeikis <SarunasMedeikis@users.noreply.github.com>
2021-02-04 16:42:40 +0200

    Final basic calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useReducer } from "react";
 
 function App() {
+  const initialState = { value: 0, evaluation: "" };
+
+  function calculateStr(userInput) {
+    return new Function("return " + userInput)();
+  }
+
+  function stateReducer(state, action) {
+    switch (action.type) {
+      case "0":
+        return { ...state, evaluation: `${state.evaluation}0` };
+      case "1":
+        return { ...state, evaluation: `${state.evaluation}1` };
+      case "2":
+        return { ...state, evaluation: `${state.evaluation}2` };
+      case "3":
+        return { ...state, evaluation: `${state.evaluation}3` };
+      case "4":
+        return { ...state, evaluation: `${state.evaluation}4` };
+      case "5":
+        return { ...state, evaluation: `${state.evaluation}5` };
+      case "6":
+        return { ...state, evaluation: `${state.evaluation}6` };
+      case "7":
+        return { ...state, evaluation: `${state.evaluation}7` };
+      case "8":
+        return { ...state, evaluation: `${state.evaluation}8` };
+      case "9":
+        return { ...state, evaluation: `${state.evaluation}9` };
+      case "+":
+        return { ...state, evaluation: `${state.evaluation}+` };
+      case "-":
+        return { ...state, evaluation: `${state.evaluation}-` };
+      case "*":
+        return { ...state, evaluation: `${state.evaluation}*` };
+      case "/":
+        return { ...state, evaluation: `${state.evaluation}/` };
+      case "C":
+        return { ...state, evaluation: `` };
+      case "=":
+        return {
+          ...state,
+          evaluation: calculateStr(state.evaluation),
+        };
+      default:
+        return state;
+    }
+  }
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+
   const buttonsArr = [
     "0",
     ".",
@@ -21,21 +70,23 @@ function App() {
     "/",
     "C",
   ];
-  let track;
   return (
     <div className="App">
       <div className="App-header">
         <p>Calculator</p>
         <div className="Calculator-styles">
           {buttonsArr.map((symbol, index) => {
-            console.log(index);
             return (
-              <button className="button" type="button">
+              <button
+                onClick={() => dispatch({ type: symbol })}
+                className="button"
+                type="button"
+              >
                 {symbol}
               </button>
             );
           })}
-          <input type="text" id="lname" name="lname" value="0" />
+          <input type="text" name="resultTab" value={state.evaluation} />
         </div>
       </div>
     </div>
